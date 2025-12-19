4459e6c78a9cef6cc772df028238d0dc43c9701a
lvadegre <lvadegre>
2025-12-19 16:29:01 +0100

    test react

---
+++ mainwebsite-react/src/App.jsx
@@ -0,0 +1,74 @@
+export default function App() {
+  return (
+    <div>
+      <h1>index de mes projets universitaires</h1>
+      <ul>
+        <li>
+          base de donnée
+          <ul>
+            <li>
+              <a href="/Content/univ/bdd_spotifi.html">
+                gestion de spotifi (parodie de spotify)
+              </a>{" "}
+              : (L2 - 2023)
+            </li>
+          </ul>
+        </li>
+
+        <br />
+
+        <li>
+          algorithmique programmation (ocaml)
+          <ul><li>tetris : (L1 - 2020)</li></ul>
+          <ul><li>analyse de mot de passe : (L2 - 2023)</li></ul>
+        </li>
+
+        <br />
+
+        <li>
+          theorie du language (yacc/lex)
+          <ul><li>analyse preface d'un livre (L2 - 2022)</li></ul>
+          <ul><li>analyse de ticket de concert (L2 - 2023)</li></ul>
+        </li>
+
+        <br />
+
+        <li>poo-ihm (java)</li>
+
+        <li>
+          programmation c
+          <ul><li>clash royale : (L2 - 2022)</li></ul>
+          <ul><li>ligne de bus : (L2 - 2023)</li></ul>
+        </li>
+
+        <li>
+          tech web
+          <ul>
+            <li>
+              <a href="https://valumane.github.io/siteweb/index.html">
+                site web de presentation d'une collection
+              </a>{" "}
+              : (L1 - 2020)
+            </li>
+          </ul>
+        </li>
+
+        <li>gestion de projet</li>
+      </ul>
+
+      <h1>index de mes projets personelle</h1>
+      <ul>
+        <li><a href="https://valumane.github.io/matrice/calmath.html">calcmath</a></li>
+        <li><a href="https://valumane.github.io/syracuse/syracuse.html">syracuse</a></li>
+        <li><a href="https://valumane.github.io/hypixelApi/">hypixel api</a></li>
+
+        <br />
+
+        <p>autre projet pas tres interessant mais "fun ?"</p>
+        <li><a href="/Content/animation/distortion.html">distorition</a></li>
+        <li><a href="/Content/animation/win_screen_saver.html">windows screen saver</a></li>
+        <li><a href="/Content/game/roaddrive/roadtest.html">roadtest</a></li>
+      </ul>
+    </div>
+  );
+}
