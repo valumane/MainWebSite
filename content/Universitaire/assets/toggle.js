5a543ad9796c5505d1a1014dc3cca5ae42cff9b2
lvadegre <lvadegre>
2025-12-21 20:33:57 +0100

    u : bdd jalon1/2 download

--- content/Universitaire/assets/toggle.js
+++ content/Universitaire/assets/toggle.js
@@ -1,14 +1,11 @@
-function toggle(div_id, div_to_toggle) {
-    const sujet = document.getElementById(div_id);
+function toggle(div_to_toggle) {
     const pdf = document.getElementById(div_to_toggle);
 
     const isHidden = window.getComputedStyle(pdf).display === "none";
 
     if (isHidden) {
         pdf.style.display = "block";
-        sujet.textContent = "sujet (masquer)";
     } else {
         pdf.style.display = "none";
-        sujet.textContent = "sujet (afficher)";
     }
 }
